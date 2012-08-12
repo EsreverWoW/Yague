01dbbb5f3bd553687dbc896d2b75e0a21e06a364
baanano <baanano@a5ebc12b-c96f-4ac6-940d-07b691108a81>
2012-08-12 22:04:21 +0000

    commit

---
+++ MoneyDisplay.lua
@@ -0,0 +1,78 @@
+-- ***************************************************************************************************************************************************
+-- * MoneyDisplay.lua                                                                                                                                *
+-- ***************************************************************************************************************************************************
+-- * Money frame                                                                                                                                     *
+-- ***************************************************************************************************************************************************
+-- * 0.4.1 / 2012.07.16 / Baanano: Rewritten                                                                                                         *
+-- ***************************************************************************************************************************************************
+
+local addonInfo, InternalInterface = ...
+local addonID = addonInfo.identifier
+local PublicInterface = _G[addonID]
+
+local MFloor = math.floor
+local MMax = math.max
+local UICreateFrame = UI.CreateFrame
+local tostring = tostring
+
+function PublicInterface.MoneyDisplay(name, parent)
+	local bMoneyDisplay = UICreateFrame("Frame", name, parent)
+	
+	local silverText = UICreateFrame("Text", bMoneyDisplay:GetName() .. ".SilverText", bMoneyDisplay)
+	local goldText = UICreateFrame("Text", bMoneyDisplay:GetName() .. ".GoldText", bMoneyDisplay)
+	local platinumText = UICreateFrame("Text", bMoneyDisplay:GetName() .. ".PlatinumText", bMoneyDisplay)
+	local silverTexture = UICreateFrame("Texture", bMoneyDisplay:GetName() .. ".SilverTexture", bMoneyDisplay)
+	local goldTexture = UICreateFrame("Texture", bMoneyDisplay:GetName() .. ".GoldTexture", bMoneyDisplay)
+	local platinumTexture = UICreateFrame("Texture", bMoneyDisplay:GetName() .. ".PlatinumTexture", bMoneyDisplay)
+
+	silverText:SetPoint("CENTERRIGHT", bMoneyDisplay, "CENTERRIGHT", -16, 0)
+
+	goldText:SetPoint("CENTERRIGHT", bMoneyDisplay, "CENTERRIGHT", -54, 0)
+
+	platinumText:SetPoint("CENTERRIGHT", bMoneyDisplay, "CENTERRIGHT", -92, 0)
+	
+	silverTexture:SetPoint("CENTERRIGHT", bMoneyDisplay, "CENTERRIGHT", 0, 0)
+	silverTexture:SetTextureAsync(addonID, "Textures/CoinSilver.png")
+	
+	goldTexture:SetPoint("CENTERRIGHT", bMoneyDisplay, "CENTERRIGHT", -38, 0)
+	goldTexture:SetTextureAsync(addonID, "Textures/CoinGold.png")
+	
+	platinumTexture:SetPoint("CENTERRIGHT", bMoneyDisplay, "CENTERRIGHT", -76, 0)
+	platinumTexture:SetTextureAsync(addonID, "Textures/CoinPlatinum.png")
+	
+	local value = 0
+	
+	local function ResetMoneyFrame()
+		local silver, gold, platinum = value % 100, MFloor(value / 100) % 100, MFloor(value / 10000)
+
+		local displayPlatinum = platinum > 0
+		local displayGold = displayPlatinum or gold > 0
+		local displaySilver = displayGold or silver > 0
+		
+		silverTexture:SetVisible(displaySilver)
+		silverText:SetVisible(displaySilver)
+		silverText:SetText(tostring(silver))
+
+		goldTexture:SetVisible(displayGold)
+		goldText:SetVisible(displayGold)
+		goldText:SetText(tostring(gold))
+
+		platinumTexture:SetVisible(displayPlatinum)
+		platinumText:SetVisible(displayPlatinum)
+		platinumText:SetText(tostring(platinum))
+	end
+	
+	function bMoneyDisplay:GetValue()
+		value = MFloor(value or 0)
+		return value
+	end
+	
+	function bMoneyDisplay:SetValue(newValue)
+		value = MFloor(MMax(newValue or 0, 0))
+		ResetMoneyFrame()
+	end
+	
+	ResetMoneyFrame()
+	
+	return bMoneyDisplay
+end
